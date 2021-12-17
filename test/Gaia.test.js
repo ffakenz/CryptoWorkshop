b6a77c7fd4e71cd49c3d999b00b9d56c812e1f95
Franco Testagrossa <franco.testagrossa@gmail.com>
2021-12-17 23:14:41 +0100

    replace chai with truffle-assertions

--- test/Gaia.test.js
+++ test/Gaia.test.js
@@ -1,9 +1,8 @@
 const NFTicket = artifacts.require("NFTicket");
 const GaiaStore = artifacts.require("GaiaStore");
 
-const chai = require("./chaisetup.js");
+const truffleAssert = require('truffle-assertions');
 const BN = web3.utils.BN;
-const expect = chai.expect;
 
 contract("Gaia Test", function (accounts) {
     const [deployer, recipient, other, unknown] = accounts;
@@ -18,8 +17,7 @@ contract("Gaia Test", function (accounts) {
         await this.nfticket.createTicket(ticketId, this.gaiaStore.address, { from: deployer });
 
         // check gaia has nft ticket balance
-        const gaiaStoreNFTBalance = await this.nfticket.balanceOf(this.gaiaStore.address);
-        expect(gaiaStoreNFTBalance).to.be.bignumber.equal('1');
+        assert.equal(await this.nfticket.balanceOf(this.gaiaStore.address), 1);
     });
 
     it("create event", async () => {
@@ -31,7 +29,7 @@ contract("Gaia Test", function (accounts) {
 
         // check event exists
         const created = await this.gaiaStore.getEvent(eventId);
-        expect(created.status).to.be.bignumber.equal('1');
+        assert.equal(created.status, 1);
     });
 
     it("reject unknown buyer", async () => {
@@ -42,8 +40,10 @@ contract("Gaia Test", function (accounts) {
         await this.gaiaStore.createEvent(eventId, 311221, 2, [ticketId]);
 
         // reject buy due to unknown buyer
-        const maybeTicket = await this.gaiaStore.buyTicket(eventId, { from: unknown, value: "2", gasPrice: 0 });
-        await expect(maybeTicket).to.eventually.be.rejected;
+        await truffleAssert.reverts(
+            this.gaiaStore.buyTicket(eventId, { from: unknown, value: "2", gasPrice: 0 }),
+            "user not allowed"
+        );
     });
 
     it("reject buy due to insufficient funds", async () => {
@@ -54,8 +54,10 @@ contract("Gaia Test", function (accounts) {
         await this.gaiaStore.createEvent(eventId, 311221, 2, [ticketId]);
 
         // reject buy due to insufficient funds
-        const maybeTicket = await this.gaiaStore.buyTicket(eventId, { from: recipient, value: "1", gasPrice: 0 });
-        await expect(maybeTicket).to.eventually.be.rejected;
+        await truffleAssert.reverts(
+            this.gaiaStore.buyTicket(eventId, { from: recipient, value: "1", gasPrice: 0 }),
+            "not enough money"
+        );
     });
 
     it("reject buy due to not enough tickets", async () => {
@@ -64,8 +66,10 @@ contract("Gaia Test", function (accounts) {
         await this.gaiaStore.createEvent(eventId, 311221, 2, []);
 
         // reject buy due to not enough tickets
-        const maybeTicket = await this.gaiaStore.buyTicket(eventId, { from: recipient, value: "2", gasPrice: 0 });
-        await expect(maybeTicket).to.eventually.be.rejected;
+        await truffleAssert.reverts(
+            this.gaiaStore.buyTicket(eventId, { from: recipient, value: "2", gasPrice: 0 }),
+            "revert not enough tickets"
+        );
     });
 
     it("sell ticket to whitelisted customer", async () => {
@@ -76,15 +80,15 @@ contract("Gaia Test", function (accounts) {
         await this.gaiaStore.createEvent(eventId, 311221, 2, [ticketId]);
 
         // check gaia has balance 0
-        expect(await web3.eth.getBalance(this.gaiaStore.address)).to.be.bignumber.equal('0');
+        assert.equal(await web3.eth.getBalance(this.gaiaStore.address), 0);
 
         // sell ticket to whitelisted customer
         await this.gaiaStore.buyTicket(eventId, { from: recipient, value: "2", gasPrice: 0 });
         
         // check customer has nft balance
-        expect(await this.nfticket.balanceOf(recipient)).to.be.bignumber.equal('1');
+        assert.equal(await this.nfticket.balanceOf(recipient), 1);
         
         // check gaia has balance 2
-        expect(await web3.eth.getBalance(this.gaiaStore.address)).to.be.bignumber.equal('2');
+        assert.equal(await web3.eth.getBalance(this.gaiaStore.address), 2);
     });
 });
